819da72bbdcbbdba18b2a98fcfaba44ad04b04d8
EzraArya <ezrawijaya10@gmail.com>
2022-12-14 20:48:22 +0700

    feat: adding function add delete list search book

--- lib/function.h
+++ lib/function.h
@@ -2,320 +2,122 @@
 #include <string.h>
 #include <stdlib.h>
 
-void account (void);
-void accountcreated (void);
-void login(void);
-void loginsu (void);
-void logout (void);
-void registermenu(void);
-void mainmenu(void);
-void welcoming (void);
+void addBook();
+void deleteBook();
+void listBook();
+void searchBook();
 
-struct pass {
-    char username[50];
-    char fname[50];
-    char lname[50];
-    char phone_number[50];
+struct books
+{
+    int id;
+    char book_name[1001];
+    char author[1001];
 };
 
-struct userpass{
-    char password[50];
-};
+void addBook()
+{
+    FILE *fptr;
+    struct books book;
 
-void gotoxy(int x, int y) {
-    // Use the ANSI escape sequence "\033[y;xH" to move the cursor to the specified position
-    printf("\033[%d;%dH", y, x);
+    fptr = fopen("docs/booklist.txt", "ab");
+
+    printf("Book ID : ");
+    scanf("%s", book.id);
+    getchar();
+    printf("Book Name : ");
+    scanf("%[^\n]", book.book_name);
+    getchar();
+    printf("Author Name : ");
+    scanf("%[^\n]", book.author);
+    getchar();
+
+    fwrite(&book, sizeof(book), 1, fptr);
+    fclose(fptr);
 }
 
-void registermenu (void) {
-    system("clear");
-    int choice;
-
-    printf("\033[20;2H");
-    printf("============================================");
-    printf("\033[20;2H");
-    printf ("Library Management System ");   
-    gotoxy(34,5);
-    printf ("Binus University ");
-    gotoxy(20,7);
-    printf("============================================");
-    
-    gotoxy(34,9);
-    printf ("Account Handling ");
-
-    gotoxy(22,11);
-    printf("----= 1. Login   ");
-
-    gotoxy(22,13);
-    printf("----= 2. Register   ");
-
-    gotoxy(22,15);
-    printf("----= 0. Close Program   ");
-
-    gotoxy(20,17);
-    printf("============================================");
-        
-    gotoxy(22,19);
-    printf("----= Console :   "); 
-    scanf("%d",&choice);
-
-    switch (choice){
-
-        case 1: 
-            login();
-        break;
-
-        case 2 :
-            account();
-        break;
-
-        case 0 :
-            system("clear");
-            gotoxy(22,8);
-            printf("---= Thank you for using this app. Have a nice Day :) =---");
-            puts("\n");
-        break;
-    }
-}
-
-void account (void) {
-    char password [50];
-    int passwordlenght, i, seek = 0;
+void deleteBook()
+{
+    FILE *fptr1, *fptr2;
+    struct books book;
     char ch;
 
-    FILE *fp, *fu;
-    struct pass u1;
-    struct userpass p1;
-    struct userpass u2;
-
-    fp = fopen("username.txt","ab");
-    system("clear");
-
-    gotoxy(20,2);
-    printf("============================================");
-    gotoxy(32,4);
-    printf ("User Register Form ");
-    gotoxy(20,6);
-    printf("============================================");
-    
-    gotoxy(22,8);
-    printf("---= First name :   ");
-    scanf("%s",u1.fname);
-
-    gotoxy(22,10);
-    printf("---= Last Name  :   ");
-    scanf("%s",u1.lname);
-
-    gotoxy(22,12);
-    printf("---= username   :   ");
-    scanf("%s",u1.username);
+    int ID;
+    printf("Enter Book ID to be deleted : ");
+    scanf("%d", &ID);
     getchar();
 
-    gotoxy(22,14);
-    printf("---= password   :   ");
-    scanf("%s", password);
+    fptr1 = fopen("docs/booklist.txt", "rb");
+    fptr2 = fopen("docs/temp___.txt", "ab");
 
-    fwrite(&u1, sizeof(u1),1,fp);
-    fclose(fp);
-    
-    accountcreated();
-}
-
-
-void accountcreated (void) {
-    int i;
-    char ch;
-    system("clear");
-    printf("PLEASE WAIT....\n\nPROCESSING YOUR DATA....");
-    for (i = 0; i < 200000000; i++) {
-        i++;
-        i--;
-    }
- 
-    gotoxy(30, 10);
-    printf("ACCOUNT CREATED SUCCESSFULLY....");
-    gotoxy(0, 20);
- 
-    printf("Press enter to Back account Handling");
-    getchar();
-
-    registermenu();
-}
-
-void login (void) {
-    system("clear");
-
-    char username [50];
-    char password [50];
-
-    char ch;
-
-    FILE *fp, *fu;
-
-    struct pass u1;
-    struct userpass u2;
-
-    fp = fopen ("username.txt","rb");
-    if (fp == NULL ){
-        printf("ERROR OPENING FILE");
+    if (fptr1 == NULL)
+    {
+        printf("Can't open File");
+        exit(0);
     }
 
-    gotoxy(20,2);
-    printf("============================================");
-    gotoxy(36,4);
-    printf ("User Login ");
-    gotoxy(20,6);
-    printf("============================================");
-
-    gotoxy(22,8);
-    printf("---= Username :   ");
-    scanf("%s", username);
-
-    gotoxy(22,10);
-    printf("---= Password :   ");
-    scanf("%s", password);
-    
-
-    while (fread(&u1, sizeof(u1),1, fp)) {
-        if (strcmp(username,u1.username)== 0) {
-            loginsu();
-            mainmenu();
-            getchar();
-        }
-        
-    }
-
-    fclose(fp);
-    
-}
-
-void loginsu(void) {
-    int i;
-    FILE* fp;
-    struct pass u1;
-    system("clear");
-    printf("Fetching account details");
-    
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 25000000; j++) {
-            i++;
-            i--;
+    while (fread(&book, sizeof(book), 1, fptr1))
+    {
+        if (book.id != ID)
+        {
+            fwrite(&book, sizeof(book), 1, fptr2);
         }
     }
- 
-    gotoxy(30, 10);
-    printf("LOGIN SUCCESSFUL....");
-    gotoxy(0, 20);
-    printf("Press enter to continue");
+
+    fclose(fptr1);
+    fclose(fptr2);
+
+    remove("docs/booklist.txt");
+    rename("docs/temp___.txt", "docs/booklist.txt");
+}
+
+void listBook()
+{
+    FILE *fptr;
+    struct books book;
+    int counter = 1;
+    fptr = fopen("docs/booklist.txt", "rb");
+    while (fread(&book, sizeof(book), 1, fptr))
+    {
+        printf("%d.\n", counter++);
+        printf("Book ID : %d\n", book.id);
+        printf("Book Name = %s\n", book.book_name);
+        printf("Author Name = %s\n", book.author);
+        putchar('\n');
+    }
+    int size = 0;
+    if (fptr != NULL)
+    {
+        fseek(fptr, 0, SEEK_END);
+
+        size = ftell(fptr);
+        rewind(fptr);
+    }
+    if (size == 0)
+    {
+        printf("List is empty\n\n");
+    }
+    fclose(fptr);
+}
+
+void searchBook()
+{
+    FILE *fptr;
+    struct books book;
+    int ID;
+    printf("Book ID : ");
+    scanf("%d", &ID);
     getchar();
 
-    puts("\n");
-}
-
-void mainmenu(void) {
-
-    system ("clear");
-    int choice;
-
-    gotoxy(20,2);
-    printf("============================================");
-    gotoxy(29,4);
-    printf ("Library Management System ");   
-    gotoxy(34,5);
-    printf ("Binus University ");
-    gotoxy(20,7);
-    printf("============================================");
-    gotoxy(37,9);
-    printf ("Main Menu");
-    gotoxy(22,11);
-    printf("----= 1. Add Book   ");
-    gotoxy(22,13);
-    printf("----= 2. Delete Book   ");
-    gotoxy(22,15);
-    printf("----= 3. Show Book List ");
-    gotoxy(22,17);
-    printf("----= 0. Log Out ");
-    gotoxy(20,19);
-    printf("============================================");
-        
-    gotoxy(22,21);
-    printf("----= Console :   "); 
-    scanf("%d",&choice);
-
-    switch (choice){
-        case 1  : 
-            printf("nah belum beres cok");
-        break;
-
-        case 2 :
-            printf("Under Development");
-        break;
-
-        case 0 :
-            logout();
-        break;
-    }
-}
-
-void logout(void) {
-    system("clear");
-    printf("please wait, logging out");
- 
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 25000000; j++) {
-            for (int k = 0; k < 10; k++) {
-                i++;
-                i--;
-            }
+    fptr = fopen("docs/booklist.txt", "rb");
+    while (fread(&book, sizeof(book), 1, fptr))
+    {
+        if (book.id == ID)
+        {
+            printf("Book found!\n");
+            printf("Book ID : %d\n", book.id);
+            printf("Book Name = %s\n", book.book_name);
+            printf("Author Name = %s\n", book.author);
         }
-        printf(".");
     }
- 
-    gotoxy(30, 10);
-    printf("Sign out successfully..\n");
- 
-    gotoxy(0, 20);
-    printf("press any key to continue..");
- 
-    getchar();
-    registermenu();
-}
-
-void welcoming (void) {
-    // system("clear");
-    // printf("Syncronizing system, please wait");
- 
-    // for (int i = 0; i < 10; i++) {
-    //     for (int j = 0; j < 25000000; j++) {
-    //         i++;
-    //         i--;
-    //     }
-    //     printf(".");
-    // }
- 
-    // gotoxy(30, 10);
-    // printf("Syncronize system successfully..\n");
-
-    // gotoxy(0, 20);
-    // printf("press any key to continue..");
- 
-    // getchar();
-
-    system("clear");
-
-    gotoxy(20,2);
-    printf("============================================");
-    gotoxy(20,4);
-    printf ("---= Welcome TO                         =---");   
-    gotoxy(20,5);
-    printf ("---= Library Management System          =---");
-    gotoxy(20,6);
-    printf ("---= Binus Universitry                  =---");
-    gotoxy(20,8);
-    printf("============================================");
-    gotoxy(0, 12);
-    printf("press any key to continue..");
-    getchar();
-
-    registermenu();
+    fclose(fptr);
 }
