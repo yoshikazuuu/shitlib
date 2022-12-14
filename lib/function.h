51efc46e917a98be5b21b39d01a96962cea5f9c3
Yoshikazuuu <febrianojerry@gmail.com>
2022-12-14 20:39:23 +0700

    chore: adding function declaration

--- lib/function.h
+++ lib/function.h
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+void gotoxy(int x, int y);
+
 void gotoxy(int x, int y) {
     // Use the ANSI escape sequence "\033[y;xH" to move the cursor to the specified position
     printf("\033[%d;%dH", y, x);
