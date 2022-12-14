f781ec392e2c2b33124384c8cde2e340c55c156c
rxyhn <rxyhn.dev@gmail.com>
2022-12-15 01:37:52 +0700

    chore: add book func

--- lib/function.h
+++ lib/function.h
@@ -1,123 +1,56 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+#define BUF_SIZE 65536
 
-void addBook();
-void deleteBook();
-void listBook();
-void searchBook();
+void gotoxy(int x, int y);
 
-struct books
-{
-    int id;
-    char book_name[1001];
-    char author[1001];
+struct users {
+  int ID;
+  char username[50];
+  char password[50];
+  int role;
 };
 
-void addBook()
-{
-    FILE *fptr;
-    struct books book;
-
-    fptr = fopen("docs/booklist.txt", "ab");
-
-    printf("Book ID : ");
-    scanf("%s", book.id);
-    getchar();
-    printf("Book Name : ");
-    scanf("%[^\n]", book.book_name);
-    getchar();
-    printf("Author Name : ");
-    scanf("%[^\n]", book.author);
-    getchar();
-
-    fwrite(&book, sizeof(book), 1, fptr);
-    fclose(fptr);
+void gotoxy(int x, int y) {
+  // Use the ANSI escape sequence "\033[y;xH" to move the cursor to the
+  // specified position
+  printf("\033[%d;%dH", y, x);
 }
 
-void deleteBook()
-{
-    FILE *fptr1, *fptr2;
-    struct books book;
-    char ch;
+int countLines(FILE *file) {
+  char buf[BUF_SIZE];
+  int counter = 0;
+  for (;;) {
+    size_t res = fread(buf, 1, BUF_SIZE, file);
+    if (ferror(file))
+      return -1;
 
-    int ID;
-    printf("Enter Book ID to be deleted : ");
-    scanf("%d", &ID);
-    getchar();
+    int i;
+    for (i = 0; i < res; i++)
+      if (buf[i] == '\n')
+        counter++;
 
-    fptr1 = fopen("docs/booklist.txt", "rb");
-    fptr2 = fopen("docs/temp___.txt", "ab");
+    if (feof(file))
+      break;
+  }
 
-    if (fptr1 == NULL)
-    {
-        printf("Can't open File");
-        exit(0);
-    }
-
-    while (fread(&book, sizeof(book), 1, fptr1))
-    {
-        if (book.id != ID)
-        {
-            fwrite(&book, sizeof(book), 1, fptr2);
-        }
-    }
-
-    fclose(fptr1);
-    fclose(fptr2);
-
-    remove("docs/booklist.txt");
-    rename("docs/temp___.txt", "docs/booklist.txt");
+  return counter;
 }
 
-void listBook()
-{
-    FILE *fptr;
-    struct books book;
-    int counter = 1;
-    fptr = fopen("docs/booklist.txt", "rb");
-    while (fread(&book, sizeof(book), 1, fptr))
-    {
-        printf("%d.\n", counter++);
-        printf("Book ID : %d\n", book.id);
-        printf("Book Name = %s\n", book.book_name);
-        printf("Author Name = %s\n", book.author);
-        putchar('\n');
-    }
-    int size = 0;
-    if (fptr != NULL)
-    {
-        fseek(fptr, 0, SEEK_END);
+int isExists(FILE *file, char username[]) {
+  struct users temp;
 
-        size = ftell(fptr);
-        rewind(fptr);
+  while (fscanf(file, "%d %s %s %d", &temp.ID, temp.username, temp.password,
+                &temp.role) != EOF) {
+    printf("%s %s\n", temp.username, username);
+    if (!strcmp(temp.username, username)) {
+      system("clear");
+      printf("Username already exists. Please try again.\n");
+      getchar();
+      return 1;
     }
-    if (size == 0)
-    {
-        printf("List is empty\n\n");
-    }
-    fclose(fptr);
-}
-
-void searchBook()
-{
-    FILE *fptr;
-    struct books book;
-    int ID;
-    printf("Book ID : ");
-    scanf("%d", &ID);
-    getchar();
-
-    fptr = fopen("docs/booklist.txt", "rb");
-    while (fread(&book, sizeof(book), 1, fptr))
-    {
-        if (book.id == ID)
-        {
-            printf("Book found!\n");
-            printf("Book ID : %d\n", book.id);
-            printf("Book Name = %s\n", book.book_name);
-            printf("Author Name = %s\n", book.author);
-        }
-    }
-    fclose(fptr);
+  }
+  fclose(file);
+  return 0;
 }
