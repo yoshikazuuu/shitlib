493cd77511d348bf882c5546dc1929b79d4f8d67
Yoshikazuuu <febrianojerry@gmail.com>
2022-12-15 23:56:26 +0700

    fix: login page

--- main.c
+++ main.c
@@ -11,22 +11,23 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char** argv) {
-    if (argc < 2) {
-        welcomePage();
-        system("clear");
-        printf("You've attemped to login as user.\n");
-        userOptions();
-    }
-    else if (argc == 2 && !strcmp(argv[1], "admin")) {
-        welcomePage();
-        system("clear");
-        printf("You've attemped to login as admin.\n");
-        adminLogin();
-    }
-    else {
-        printf("\nArgument not found.\nAvailable argument:\n1. (none) -> login as user.\n2. admin -> login as admin.\n\n");
-    }
+int main(int argc, char **argv) {
+  if (argc < 2) {
+    welcomePage();
+    system(clear);
+    printf("You've attemped to login as user.\n");
+    sleep(2);
+    userOptions();
+  } else if (argc == 2 && !strcmp(argv[1], "admin")) {
+    welcomePage();
+    system(clear);
+    printf("You've attemped to login as admin.\n");
+    sleep(2);
+    adminLogin();
+  } else {
+    printf("\nArgument not found.\nAvailable argument:\n1. (none) -> login as "
+           "user.\n2. admin -> login as admin.\n\n");
+  }
 
-    return 0;
+  return 0;
 }
